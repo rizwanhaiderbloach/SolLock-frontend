8657dbcea5fcc683fbc8b98ede76c4d665a853b5
rizwanhaiderbloach <rizwan.hdevelope@gmail.com>
2026-07-17 00:11:04 +0500

    Remove unused clusterApiUrl import

--- src/WalletContextProvider.tsx
+++ src/WalletContextProvider.tsx
@@ -1,4 +1,3 @@
-import { clusterApiUrl } from '@solana/web3.js'
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets'
