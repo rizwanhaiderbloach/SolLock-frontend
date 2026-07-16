bf7237bd2d893d36d1807bd11d5e35373a0ef6f9
rizwanhaiderbloach <rizwan.hdevelope@gmail.com>
2026-07-16 23:49:29 +0500

    Use Helius devnet RPC

--- src/WalletContextProvider.tsx
+++ src/WalletContextProvider.tsx
@@ -8,7 +8,7 @@ import type { PropsWithChildren } from 'react'
 import '@solana/wallet-adapter-react-ui/styles.css'
 
 export function WalletContextProvider({ children }: PropsWithChildren) {
-  const endpoint = useMemo(() => clusterApiUrl('devnet'), [])
+  const endpoint = useMemo(() => 'https://devnet.helius-rpc.com/?api-key=34ec8d48-0450-4b58-8040-16be826a710d', [])
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [])
 
   return (
